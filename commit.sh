d41e81401b74a81631115b1883d8498e42e4acb1
Ilia Lebedev <ilebedev@mit.edu>
2017-10-19 11:00:27 -0400

    bugfix; added scripts to build ignore

--- commit.sh
+++ commit.sh
@@ -10,8 +10,8 @@ git push
 git status
 
 echo "#############"
-echo "## You should see: \""
+echo '## You should see: "'
 echo "On branch master"
-echo "Your branch is up-to-date with \'origin/master\'."
-echo "nothing to commit, working tree clean \""
+echo "Your branch is up-to-date with 'origin/master'."
+echo "nothing to commit, working tree clean '"
 echo "#############"
